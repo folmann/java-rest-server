3c31fab09285651c53b4e4de61075f912dcd20d8
alexandre <alexandre@alexandre-Inspiron-N4050>
2016-05-28 12:19:44 -0300

    novos métodos.

--- src/java/util/Util.java
+++ src/java/util/Util.java
@@ -6,6 +6,9 @@
 package util;
 
 import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import modelo.Hospedagem;
 import modelo.Voo;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -16,7 +19,7 @@ import org.json.simple.JSONObject;
  */
 public class Util {
     
-    public static JSONObject toJSON(ArrayList<Voo> list) {
+    public static JSONObject toJSONVoos(ArrayList<Voo> list) {
         JSONArray jsonArray = new JSONArray();
         JSONObject json = new JSONObject();
         list.stream().forEach(jsonArray::add);
@@ -24,5 +27,67 @@ public class Util {
         json.put("size", jsonArray.size());
         
         return json;
+    }
+    
+    public static JSONObject toJSONHospedagens(ArrayList<Hospedagem> list) {
+        JSONArray jsonArray = new JSONArray();
+        JSONObject json = new JSONObject();
+        list.stream().forEach(jsonArray::add);
+        json.put("hospedagens", jsonArray);
+        json.put("size", jsonArray.size());
+        
+        return json;
+    }
+    
+    public static ConcurrentMap<String, Hospedagem> iniciarHospedagens() {
+        ConcurrentMap<String, Hospedagem> hosp;
+        hosp = new ConcurrentHashMap<>();
+        
+        Hospedagem h = new Hospedagem();
+        h.setDestino("saopaulo");
+        h.adicionarQuartosPorData(10, "2016-06-01");
+        h.adicionarQuartosPorData(10, "2016-06-02");
+        h.adicionarQuartosPorData(10, "2016-06-03");
+        hosp.put(h.getId(), h);
+        
+        h = new Hospedagem();
+        h.setDestino("catarina");
+        h.adicionarQuartosPorData(20, "2016-06-01");
+        h.adicionarQuartosPorData(20, "2016-06-02");
+        h.adicionarQuartosPorData(20, "2016-06-03");
+        hosp.put(h.getId(), h);
+        
+        return hosp;
+    }
+    
+    public static ConcurrentMap<String, Voo> iniciarVoos() {
+        ConcurrentMap<String, Voo> voos;
+        voos = new ConcurrentHashMap<>();
+        
+        Voo v = new Voo();
+        v.setOrigem("curitiba");
+        v.setDestino("saopaulo");
+        v.setData("2016-06-01");
+        voos.put(v.getId(), v);
+        
+        v = new Voo();
+        v.setOrigem("saopaulo");
+        v.setDestino("curitiba");
+        v.setData("2016-06-01");
+        voos.put(v.getId(), v);
+        
+        v = new Voo();
+        v.setOrigem("saopaulo");
+        v.setDestino("curitiba");
+        v.setData("2016-06-02");
+        voos.put(v.getId(), v);
+        
+        v = new Voo();
+        v.setOrigem("saopaulo");
+        v.setDestino("curitiba");
+        v.setData("2016-06-03");
+        voos.put(v.getId(), v);
+        
+        return voos;
     }
 }
